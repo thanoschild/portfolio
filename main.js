90e6ec458772c1bcf1a71025fb6e13cc8bb1f703
godspell <sumeethaldar731@gmail.com>
2022-06-08 14:38:30 +0530

    make all the changes

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
+$( document ).ready(function() {
 $('a[href*="#"]').on('click', function (e) {
 	e.preventDefault();
 
 	$('html, body').animate({
 		scrollTop: $($(this).attr('href')).offset().top
 	}, 0, 'linear');
+});
+
+$(".mobile-menu").click(function () {
+	var data = $(".menu ul").attr("data-visible");
+	if (data == "false") {
+		$(".menu ul").attr("data-visible", true);
+		$(".mobile-menu").attr("area-expanded", true);
+	} else {
+		$(".menu ul").attr("data-visible", false);
+		$(".mobile-menu").attr("area-expanded", false);
+	}
+});
 });
